90e1121125e58abc61e458ca4942584ef8be3819
Alan Shum <alan200994@gmail.com>
2018-06-09 13:43:59 -0700

    wrap in function

--- main.tcl
+++ main.tcl
@@ -1,8 +1,5 @@
 package require Plotchart
 
-puts "What do you want to say?"
-set userInput [gets stdin]
-
 proc getLetterOccurrence {srcString} {
   array set occurrenceList [list]
   set srcStringLength [string length $srcString]
@@ -26,9 +23,6 @@ proc getLetterOccurrence {srcString} {
   return [array get occurrenceList]
 }
 
-array set letterOccurrenceList [getLetterOccurrence $userInput]
-puts "\[DEBUG\] letterOccurrenceList = [array get letterOccurrenceList]"
-
 proc findMax {valueList} {
   array set valueArray $valueList
   set max 0
@@ -41,32 +35,38 @@ proc findMax {valueList} {
   return $max
 }
 
-set alphabets [list A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
-set maxPercentage [findMax [array get letterOccurrenceList]]
-set yRange [list 0 $maxPercentage 1]
-set xRange [list 0 [llength $alphabets] ""]
+proc plotOccurrenceChart {occurrenceList} {
+  array set occurrenceArray $occurrenceList
+  set alphabets [list A B C D E F G H I J K L M N O P Q R S T U V W X Y Z]
+  set maxPercentage [findMax [array get occurrenceArray]]
+  set yRange [list 0 $maxPercentage 1]
+  set xRange [list 0 [llength $alphabets] ""]
 
-pack [canvas .c -width 1280 -height 720 -bg white];
-Plotchart::plotstyle configure default histogram leftaxis format "%.0f%%"
-set p [::Plotchart::createHistogram .c $xRange $yRange \
-  -xlabels $alphabets
-];
+  pack [canvas .c -width 1280 -height 720 -bg white];
+  Plotchart::plotstyle configure default histogram leftaxis format "%.0f%%"
 
-$p dataconfig data -style filled -fillcolour #50ACC4 -colour "";
+  set chart [::Plotchart::createHistogram .c $xRange $yRange -xlabels $alphabets];
+  $chart dataconfig data -style filled -fillcolour #50ACC4 -colour "";
 
-for {set i 0} {$i < [llength $alphabets]} {incr i} {
-  set letter [lindex $alphabets $i]
-  set chartIndex [expr {$i + 1}]
+  for {set i 0} {$i < [llength $alphabets]} {incr i} {
+    set letter [lindex $alphabets $i]
+    set chartIndex [expr {$i + 1}]
 
-  if ([info exists letterOccurrenceList($letter)]) {
-    set occurrence $letterOccurrenceList($letter)
-  } else {
-    set occurrence 0
+    if ([info exists occurrenceArray($letter)]) {
+      set occurrence $occurrenceArray($letter)
+    } else {
+      set occurrence 0
+    }
+
+    puts "\[DEBUG\]letter plotData: $letter $chartIndex $occurrence";
+    $chart plot data $chartIndex $occurrence;
   }
-
-  puts "letter plotData: $letter $chartIndex $occurrence";
-  $p plot data $chartIndex $occurrence;
 }
 
-# # # debugging
-# exit
+puts "What do you want to say?"
+set userInput [gets stdin]
+
+array set letterOccurrenceList [getLetterOccurrence $userInput]
+puts "\[DEBUG\] letterOccurrenceList = [array get letterOccurrenceList]"
+
+plotOccurrenceChart [array get letterOccurrenceList]
