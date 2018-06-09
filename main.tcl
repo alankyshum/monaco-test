dee8034ca67c80f0aaf26963e3b721a948f2f11f
Alan Shum <alan200994@gmail.com>
2018-06-09 10:59:00 -0700

    wrap in proc

--- main.tcl
+++ main.tcl
@@ -1,31 +1,31 @@
 puts "What do you want to say?"
-
 set userInput "aaaabbcc"
 # set userInput [gets stdin]
 
 array set letterOccurrenceList [list]
-set userInputLength [string length $userInput]
 
-puts "\[DEBUG\]: String '$userInput' with length $userInputLength"
+proc getLetterOccurrence {userInput} {
+  set userInputLength [string length $userInput]
 
-for {set i 0} {$i < [string length $userInput]} {incr i} {
-  set letter [string index $userInput $i];
+  for {set i 0} {$i < [string length $userInput]} {incr i} {
+    set letter [string index $userInput $i];
 
-  if ([info exists letterOccurrenceList($letter)]) {
-    incr letterOccurrenceList($letter);
-  } else {
-    set letterOccurrenceList($letter) 1;
+    if ([info exists letterOccurrenceList($letter)]) {
+      incr letterOccurrenceList($letter);
+    } else {
+      set letterOccurrenceList($letter) 1;
+    }
   }
+
+  foreach {letter occurrence} [array get letterOccurrenceList] {
+    set occurrence [expr double($occurrence)]
+    set letterOccurrenceList($letter) [expr {$occurrence / $userInputLength * 100}]
+  }
+
+  return [array get letterOccurrenceList]
 }
 
-puts "\[DEBUG\]: [array get letterOccurrenceList]"
-
-foreach {letter occurrence} [array get letterOccurrenceList] {
-  set occurrence [expr double($occurrence)]
-  set letterOccurrenceList($letter) [expr {$occurrence / $userInputLength * 100}]
-}
-
-puts "\[DEBUG\]: [array get letterOccurrenceList]"
+puts [getLetterOccurrence $userInput]
 
 # # get percentages
 # Object.keys(letterOccurrenceList).forEach(letter => {
